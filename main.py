0bd2355126ffabce99ca95a835fc30c3acb0a33e
ykaa <pepahurka3@gmail.com>
2026-04-05 11:16:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}!')
+
+    async def on_message(self,message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = Client(intents=intents)
+client.run('YOUR_BOT_TOKEN')
